39b62d5d415f7f569037c84e2e2be94cc41812a0
steve-chavez <stevechavezast@gmail.com>
2018-01-23 21:45:19 -0500

    Integrate buttons with hardcoded charges

--- openresty/lualib/user_code/charge_customer.lua
+++ openresty/lualib/user_code/charge_customer.lua
@@ -1,18 +1,35 @@
 local jwt = require 'resty.jwt'
 local jwt_secret = os.getenv('JWT_SECRET')
 
-local is_authorized, user_id = (function(authorization)
+local is_authorized, user_id = (function()
+  -- Hardcoded for now, uncomment this later
   --local authorization = ngx.var.http_authorization
+  local authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJyb2xlIjoid2VidXNlciJ9.uSsS2cukBlM6QXe4Y0H90fsdkJSGcle9b7p_kMV1Ymk"
   if not authorization then return false end
 
   local _, _, token = string.find(authorization, "Bearer%s+(.+)")
   local jwt_obj = jwt:verify(jwt_secret, token)
   return jwt_obj and jwt_obj.valid, jwt_obj.payload.user_id
-end)("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJyb2xlIjoid2VidXNlciJ9.uSsS2cukBlM6QXe4Y0H90fsdkJSGcle9b7p_kMV1Ymk")
+end)()
 
 if not is_authorized then
   ngx.status = ngx.HTTP_UNAUTHORIZED
   ngx.exit(ngx.HTTP_UNAUTHORIZED)
+end
+
+local amount, description = (function()
+  local item = ngx.req.get_uri_args().item
+  if item == "gpl" then return 1999, "GPL License"
+  elseif item == "mit" then return 9999, "MIT License"
+  elseif item == "regular-sub" then return 1999, "subZero regular subscription"
+  elseif item == "enterprise-sub" then return 2999, "subZero Enterprise subscription"
+  else return nil, nil
+  end
+end)()
+
+if not amount then
+  ngx.status = ngx.HTTP_BAD_REQUEST
+  ngx.exit(ngx.HTTP_BAD_REQUEST)
 end
 
 local stripe_secret_key = os.getenv('STRIPE_SECRET_KEY')
@@ -52,9 +69,9 @@ local res = http_client:request_uri(stripe_endpoint .. '/charges', {
       ['Authorization'] = stripe_auth_header,
   },
   body = ngx.encode_args({
-      amount = 9900,
+      amount = amount,
       currency = 'usd',
-      description = 'Khumbu Icefall Service',
+      description = description,
       customer = stripe_customer_id
   })
 })
