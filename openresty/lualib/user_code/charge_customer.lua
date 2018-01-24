5d2b61c90bdbb1a36304dff6b7a83311c7638cd7
steve-chavez <stevechavezast@gmail.com>
2018-01-23 20:38:42 -0500

    Hardcode Bearer token

--- openresty/lualib/user_code/charge_customer.lua
+++ openresty/lualib/user_code/charge_customer.lua
@@ -1,14 +1,14 @@
 local jwt = require 'resty.jwt'
 local jwt_secret = os.getenv('JWT_SECRET')
 
-local is_authorized, user_id = (function()
-  local authorization = ngx.var.http_authorization
+local is_authorized, user_id = (function(authorization)
+  --local authorization = ngx.var.http_authorization
   if not authorization then return false end
 
   local _, _, token = string.find(authorization, "Bearer%s+(.+)")
   local jwt_obj = jwt:verify(jwt_secret, token)
   return jwt_obj and jwt_obj.valid, jwt_obj.payload.user_id
-end)()
+end)("Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJyb2xlIjoid2VidXNlciJ9.uSsS2cukBlM6QXe4Y0H90fsdkJSGcle9b7p_kMV1Ymk")
 
 if not is_authorized then
   ngx.status = ngx.HTTP_UNAUTHORIZED
@@ -33,8 +33,7 @@ local res, err = http_client:request_uri(stripe_endpoint .. '/customers', {
   body = ngx.encode_args({
       ['metadata[user_id]'] = user_id,
       source = args.stripeToken,
-      description = args.description,
-      email = args.email
+      email = args.stripeEmail
   })
 })
 
